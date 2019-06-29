8b4cb8b14476314d78166af5d265b52e9558002e
Strik3agle98 <can_saelo@hotmail.com>
2019-06-29 18:58:29 +0700

    added JSON in growth, new growth function

--- src/utills/growth.js
+++ src/utills/growth.js
@@ -1,6 +1,11 @@
 //n can be any number >= 1
+function growth(nClick){
+    grow = calculate(nClick);
+    return '{"name": "tree'+ grow + '", "src": "image/img' + grow + '.svg"}';
+}
 
-function growth(n){
-    var ans = 15 * Math.log(n)/Math.log(20);
+function calculate(n){
+    var ans = 4 * Math.log(n)/Math.log(2);
     return Math.floor(ans)
 }
+
