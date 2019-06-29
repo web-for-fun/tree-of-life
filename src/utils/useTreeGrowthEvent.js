2b335e3f19ec5034061f648655b6b587f34247ff
Ryan-Willpower <surasak.c.work@gmail.com>
2019-06-29 19:24:42 +0700

    add event hooks

---
+++ src/utils/useTreeGrowthEvent.js
@@ -0,0 +1,16 @@
+//n can be any number >= 1
+import React from 'react'
+
+function useTreeGrowthEvent(totalInteract) {
+  const [currentPic, setCurrentPic] = React.useState(1)
+  const [nextUserInteractCount, setNextUserInteractCount] = React.useState(2)
+
+  if (totalInteract === nextUserInteractCount) {
+    setCurrentPic(currentPic + 1)
+    setNextUserInteractCount(Math.ceil(nextUserInteractCount * 1.2))
+  }
+
+  return currentPic
+}
+
+export default useTreeGrowthEvent
