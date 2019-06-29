6978b146a3c9688828d57598aaa82eaaeb5c22a2
Ryan-Willpower <surasak.c.work@gmail.com>
2019-06-29 19:24:16 +0700

    growth test

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import styled from '@emotion/styled'
 import Layout from './components/layout'
+import { useInteractEvent, useTreeGrowthEvent } from './utils'
 
 const MainApp = styled.div`
   color: pink;
 `
 
 function App() {
+  const totalInteract = useInteractEvent()
+  const currentPic = useTreeGrowthEvent(totalInteract)
+
   return (
     <Layout>
       <MainApp>Hello World!</MainApp>
+      <div>you interact: {totalInteract} time(s)</div>
     </Layout>
   )
 }
